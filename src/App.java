623f41636c56d83a0d1bbcb49735ba19d4055792
benesja4 <benes.jakub1@gmail.com>
2023-10-04 13:04:27 +0200

    lesson 03

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
 import model.Line;
-import rasterize.LineRasterizer;
-import rasterize.LineRasterizerGraphics;
-import rasterize.LineRasterizerTrivial;
-import rasterize.RasterBufferedImage;
+import model.Polygon;
+import rasterize.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,74 +17,75 @@ import model.Point;
 
 public class App {
 
-	private JPanel panel;
-	private RasterBufferedImage raster;
-	private LineRasterizer lineRasterizer;
+    private JPanel panel;
+    private RasterBufferedImage raster;
+    private LineRasterizer lineRasterizer;
+    private PolygonRasterizer polygonRasterizer;
+    private Polygon polygon;
 
-	public App(int width, int height) {
-		JFrame frame = new JFrame();
+    public App(int width, int height) {
+        JFrame frame = new JFrame();
 
-		frame.setLayout(new BorderLayout());
+        frame.setLayout(new BorderLayout());
+        frame.setTitle("UHK FIM PGRF : " + this.getClass().getName());
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		frame.setTitle("UHK FIM PGRF : " + this.getClass().getName());
-		frame.setResizable(true);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        raster = new RasterBufferedImage(width, height);
+		lineRasterizer = new LineRasterizerGraphics(raster);
+        //lineRasterizer = new LineRasterizerTrivial(raster);
+        polygonRasterizer = new PolygonRasterizer(lineRasterizer);
 
-		raster = new RasterBufferedImage(width, height);
-//		lineRasterizer = new LineRasterizerGraphics(raster);
-		lineRasterizer = new LineRasterizerTrivial(raster);
+        polygon = new Polygon();
 
-		panel = new JPanel() {
-			private static final long serialVersionUID = 1L;
+        panel = new JPanel() {
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public void paintComponent(Graphics g) {
-				super.paintComponent(g);
-				present(g);
-			}
-		};
-		panel.setPreferredSize(new Dimension(width, height));
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                present(g);
+            }
+        };
+        panel.setPreferredSize(new Dimension(width, height));
 
-		frame.add(panel, BorderLayout.CENTER);
-		frame.pack();
-		frame.setVisible(true);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
 
-		panel.requestFocus();
-		panel.requestFocusInWindow();
-		panel.addMouseMotionListener(new MouseAdapter() {
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				super.mouseDragged(e);
+        panel.requestFocus();
+        panel.requestFocusInWindow();
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                raster.clear();
 
-				raster.clear();
+                Point p = new Point(e.getX(), e.getY());
+                polygon.addPoint(p);
+                polygonRasterizer.rasterize(polygon);
 
-				Point p1 = new Point(width / 2, height / 2);
-				Point p2 = new Point(e.getX(), e.getY());
-				Line line  = new Line(p1, p2, 0xffff00);
-				lineRasterizer.rasterize(line);
+                panel.repaint();
+            }
+        });
+    }
 
-				panel.repaint();
-			}
-		});
-	}
+    public void clear(int color) {
+        raster.setClearColor(color);
+        raster.clear();
+    }
 
-	public void clear(int color) {
-		raster.setClearColor(color);
-		raster.clear();
-	}
+    public void present(Graphics graphics) {
+        raster.repaint(graphics);
+    }
 
-	public void present(Graphics graphics) {
-		raster.repaint(graphics);
-	}
+    public void start() {
+        clear(0xaaaaaa);
 
-	public void start() {
-		clear(0xaaaaaa);
+        panel.repaint();
+    }
 
-		panel.repaint();
-	}
-
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> new App(800, 600).start());
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new App(800, 600).start());
+    }
 
 }
---
+++ src/model/Polygon.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Polygon {
+    private ArrayList<Point> points;
+
+    public Polygon() {
+        this.points = new ArrayList<>();
+    }
+
+    public void addPoint(Point p) {
+        points.add(p);
+    }
+
+    public int getSize() {
+        return points.size();
+    }
+
+    public Point getPoint(int index) {
+        return points.get(index);
+    }
+
+}
